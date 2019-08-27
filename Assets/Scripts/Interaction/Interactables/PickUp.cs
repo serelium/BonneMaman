b248e60baa43e3185fd8d9072668306b12940ceb
serelium <sam.basket4life@gmail.com>
2019-08-27 03:58:51 -0400

    Cleanup unused code + Added hidden message, you can find it :)?

--- Assets/Scripts/Interaction/Interactables/PickUp.cs
+++ Assets/Scripts/Interaction/Interactables/PickUp.cs
@@ -4,18 +4,10 @@ using UnityEngine;
 
 public class PickUp : Interactable
 {
-    //public Transform HoldItem;
-
-    float throwForce = 500;
-    Vector3 objectPos;
-    float distance;
-
-    public bool canHold = true;
-    public bool isHolding = false;
-
+    private bool isHolding = false;
     private Rigidbody rb;
     private Collider collider;
-    private Player interactor;
+    private Player holder;
 
     private void Start()
     {
@@ -28,13 +20,12 @@ public class PickUp : Interactable
     {
         if (isHolding && Input.GetKeyDown(KeyCode.E))
         {
-            interactor.CanInteract = true;
+            holder.CanInteract = true;
             isHolding = false;
             rb.useGravity = true;
-            //rb.isKinematic = false;
+            rb.isKinematic = false;
             transform.parent = null;
-            interactor = null;
-            collider.isTrigger = false;
+            holder = null;
         }
     }
 
@@ -43,26 +34,16 @@ public class PickUp : Interactable
         StartCoroutine(Pickup(interactor));
     }
 
-    private void OnTriggerEnter(Collider collider)
-    {
-        if(isHolding && interactor)
-        {
-            interactor.RigidBody.velocity = Vector3.zero;
-            interactor.RigidBody.angularVelocity = Vector3.zero;
-        }
-    }
-
-    private IEnumerator Pickup(Player interactor)
+    private IEnumerator Pickup(Player holder)
     {
         yield return new WaitForEndOfFrame();
 
-        interactor.CanInteract = false;
+        holder.CanInteract = false;
         isHolding = true;
-        transform.parent = interactor.HoldPoint.transform;
+        transform.parent = holder.HoldPoint.transform;
         rb.useGravity = false;
-        //rb.isKinematic = true;
-        collider.isTrigger = true;
-        this.interactor = interactor;
+        rb.isKinematic = true;
+        this.holder = holder;
         transform.localPosition = new Vector3(0, 0, 0);
     }
 }
