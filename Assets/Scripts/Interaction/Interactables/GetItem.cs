891a1d3acb4d9ab490cd85a4912386eed3b2d236
Matthew Weidner <matthew.a.weidner@gmail.com>
2019-08-29 20:22:32 -0400

    Added footstep sfx to the Player. Added sfx and particle effects to the camera pickup and some other minor functionality to items. To see for yourself, load the test scene "Time_Puzzle_Room."

--- Assets/Scripts/Interaction/Interactables/GetItem.cs
+++ Assets/Scripts/Interaction/Interactables/GetItem.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class GetItem : Interactable
 {
+    public AudioSource audioSource;
+    public AudioClip cameraSound;
+    public AudioClip futureLensSound;
+
     public float rayLength;
     private RaycastHit hit;
     private Ray ray;
 
     private GameObject futureCamera;
+    private GameObject cameraPickUp;
 
     public override void Interact(Player interactor)
     {
@@ -21,6 +26,7 @@ public class GetItem : Interactable
     void Start()
     {
         futureCamera = GameObject.Find("FutureCamera");
+        cameraPickUp = GameObject.Find("CameraPickUp");
     }
 
     // Update is called once per frame
@@ -32,12 +38,21 @@ public class GetItem : Interactable
         {
             if(hit.collider.gameObject.tag == "CameraPickUp" && Input.GetKeyDown(KeyCode.E))
             {
-                Destroy(hit.collider.gameObject);
+                //destroys the camera after a set delay of time (delay needed for particles to play)
+                Destroy(hit.collider.gameObject, .8f);
+                //plays a sound upon pickup
+                cameraPickUp.GetComponent<ParticleSystem>().Play();
+                //allows use of the time camera (needs to be toggled off in the inspector before pickup to work)
                 GameObject.Find("Player").GetComponent<Player>().useTimeCamera = true;
+                //hides the time camera item upon pickup, giving the particles time to play
+                GameObject.Find("CameraModel").SetActive(false);
+                //turns off the view for the future camera, though you can still swap to the dimension physically. needs tweaking
                 futureCamera.SetActive(false);
+                //plays a sound clip upon pick up of the camera
+                AudioManager.Instance.Play(gameObject, "Item_PickUp");
             }
 
-            if (hit.collider.gameObject.tag == "CameraFutureLense" && Input.GetKeyDown(KeyCode.E))
+            if (hit.collider.gameObject.tag == "FutureLensePickUp" && Input.GetKeyDown(KeyCode.E))
             {
                 Destroy(hit.collider.gameObject);
                 futureCamera.SetActive(true);
